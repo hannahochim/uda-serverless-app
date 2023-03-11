02ff1cc11fe32a6f0fd31186cba8a0b7129eeb8f
hannahochim <hannahochim@gmail.com>
2023-03-11 12:03:58 +0100

    Add domain and clientId

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,7 +2,7 @@ const apiId = '';
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
-  domain: '',
-  clientId: '',
+  domain: 'dev-4ue2028uh3x8yolt.us.auth0.com',
+  clientId: 'fLm9qABsz5dE4DnAAiDLV1CdUEph4aiz',
   callbackUrl: 'http://localhost:3000/callback'
 };
